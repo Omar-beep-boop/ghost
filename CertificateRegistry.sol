33c879a5753a09da3ea3ceba58e26bd183264938
Omar-beep-boop <omarmohammedissa720@gmail.com>
2026-03-23 16:19:58 +0200

    Update CertificateRegistry.sol

--- CertificateRegistry.sol
+++ CertificateRegistry.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract CertificateRegistry {
+    mapping(bytes32 => bool) public certificates;
+
+    event CertificateAdded(bytes32 hash);
+
+    function addCertificate(bytes32 hash) public {
+        require(!certificates[hash], "Certificate already exists");
+        certificates[hash] = true;
+        emit CertificateAdded(hash);
+    }
+
+    function verifyCertificate(bytes32 hash) public view returns (bool) {
+        return certificates[hash];
+    }
+}
